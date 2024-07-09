ae237778f85950615042ae7f499d8e8aafe33a02
alvarezmxm <98833828+alvarezmxm@users.noreply.github.com>
2024-07-08 21:49:36 -0500

    Se agrego una variable con un formato .format

--- strings.py
+++ strings.py
@@ -9,3 +9,10 @@ templete = "Hola, mi nombre es " + nombre + " " + apellido
 
 #Imprimimos las variables
 print(templete)
+
+#Creacion de una variable para crar un formato sin tener que concatenar.
+#En este caso solo usamos .format() dentro de los parentesis colocamos las variables
+templete2 = "Hola, mi nombre es {} {}".format(nombre, apellido)
+
+print(templete2)
+
