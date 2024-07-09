5df28dcffc5f097f0fa0f410e333625cb25f8204
alvarezmxm <98833828+alvarezmxm@users.noreply.github.com>
2024-07-08 21:44:13 -0500

    Se agrega el archivo string.py

---
+++ strings.py
@@ -0,0 +1,11 @@
+#Creacion de una variable con "comillas dobles"
+nombre = "Alfredo"
+
+#Creacion de una variable con 'comillas simples'
+apellido = "Alvarez"
+
+#Creacion de variable para concatenar variables
+templete = "Hola, mi nombre es " + nombre + " " + apellido
+
+#Imprimimos las variables
+print(templete)
