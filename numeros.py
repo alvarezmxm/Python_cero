c2a68fe8572d94ab38ccb39aa1fd91887bdffb1b
alvarezmxm <alvarezmxm@gihup.com>
2024-07-16 23:11:37 -0500

    Se agregaron formas de como restar o sumar numeros

--- numeros.py
+++ numeros.py
@@ -1,3 +1,27 @@
 vidas = 3
 print("El usuario tiene ", vidas, "vidas")
 print("El tipo de datos de las vidas son ", type(vidas))
+
+temperatura = 12.12
+print("Estamos a una temperatura de ", temperatura)
+print("El tipo de dato de la temperatura es ", type(temperatura))
+
+suma = vidas + temperatura
+print("Es una suma entre las vidas y temperatura y es: ", suma)
+print("El tipo de dato de la suma es: ", type(suma))
+
+
+print("==============================================")
+print("==============================================")
+
+vidas2 = 2
+print("Tenemos ", vidas2, "vidas")
+print("Vamos a restar una vida colocando variable = variable -1")
+vidas2 = vidas2 -1
+print("actualemnte tenemos ", vidas2, "vida")
+print("Vamos a restar otra vida pero ahora colocando variable -=1, esta es la mas comun")
+vidas2 -=1
+print("Actualmente tenemos ", vidas2, "vidas")
+print("Vamos a sumar una vida colocando variable +=1")
+vidas2 +=1
+print("Actualmente tenemos ", vidas2, "vida")
