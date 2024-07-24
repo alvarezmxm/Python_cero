9d204d4aba012ef931703b79b6be1e8cd52df6d6
alvarezmxm <alvarezmxm@gihup.com>
2024-07-24 17:42:48 -0500

    Se agrego el archivo booleans.py

---
+++ booleans.py
@@ -0,0 +1,16 @@
+#Vamos a ver los boleanos True and False esto solamente para saber si un objeto tiene un valor
+
+
+dato = True
+print(dato)
+print(type(dato))
+
+dato = False
+print(dato)
+print(type(dato))
+
+#Podemos revertir el valor utilizando not
+
+dato = not dato
+print(dato)
+print(type(dato))
