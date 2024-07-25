3805fc766f3b9b591d07d1c8d8f277adda4f7cf6
alvarezmxm <alvarezmxm@gihup.com>
2024-07-24 21:00:09 -0500

    Se agrego el archivo ope_comparadores.py

---
+++ ope_comparadores.py
@@ -0,0 +1,29 @@
+#Operadores de comparacion
+
+# == es igual que
+print(1 == 2) #False
+print(2 == 2) #True
+
+# =! es diferetne de 
+print(1 != 2) #True
+print(2 != 2) #False
+
+# < es menor que
+print(1 < 2) #True
+print(1 < 0) #False
+print(1 < 1) #False
+
+# > es mayor que
+print(1 > 2) #Falso
+print(1 > 0) #True
+print(1 > 1) #False
+
+# <= menor o igual que
+print(1 <= 2) #True
+print(1 <= 0) #False
+print(1 <= 1) #True
+
+# >= mayor o igual
+print(1 >= 2) #False
+print(1 >= 0) #True
+print(1 >= 1) #True
