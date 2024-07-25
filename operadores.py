72c2126b9eddaf5609edddba2a05d38381b1819c
alvarezmxm <alvarezmxm@gihup.com>
2024-07-24 19:25:09 -0500

    Se agrego el archivo operadores.py

---
+++ operadores.py
@@ -0,0 +1,15 @@
+#vamo a ver los operadores aritmeticos
+
+print(10 + 1) #suma
+print(10 - 1) #resta
+print(10 * 2) #multiplicacion
+print(10 / 2) #division
+print(5 ** 2) #exponenciacion 5 a la 2 = 25
+
+#Con string se puede usar 
+print("hola" + " " + "soy" + " " + "Alfredo")
+print("Hola, " * 3)
+
+#para varias operaciones se usa los parentesis y se hace de izquierza a derecha
+
+print(2 + 1 - 3 + 10 * 2) # 2+1=3-3=0+10=10*2=20
